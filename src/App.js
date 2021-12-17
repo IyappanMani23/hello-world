2a8e3c9a276d2ae3a13859221124d74fa4ab0008
Iyappan M <iym@deloitte.com>
2021-12-17 16:55:09 +0530

    company email added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
+        <body>
+          <h1>My company</h1>     
+          <p>contact@mycompany.com</p> 
+        </body>        
         <a
           className="App-link"
           href="https://reactjs.org"
